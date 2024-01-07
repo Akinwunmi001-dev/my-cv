3af61d11a54f77e392ef38d5a22e4d748ba6b3fe
Akinwunmi001-dev <feezyfemi001@gmail.com>
2024-01-07 17:21:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function change() {
+    var x =document.getElementById("main")
+    var y = document.getElementById("sub")
+    var c= document.getElementById("bw")
+    var f= document.getElementById("pgc")
+    y.style.visibility ="visible";
+
+x.innerHTML = y.innerHTML
+}
+document.querySelector("div").classList;
